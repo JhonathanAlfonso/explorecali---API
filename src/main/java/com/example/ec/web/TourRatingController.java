4b8f6c3d41694ba04ef469f584347912963c1e5e
salajhon69 <salajhon69@gmail.com>
2021-06-14 15:37:11 -0500

    feat: finish api

--- src/main/java/com/example/ec/web/TourRatingController.java
+++ src/main/java/com/example/ec/web/TourRatingController.java
@@ -38,17 +38,6 @@ public class TourRatingController {
                 ratingDto.getComment()));
     }
 
-    private Tour verifyTour(int tourId) throws NoSuchElementException {
-        return tourRepository.findById(tourId).orElseThrow(() ->
-                new NoSuchElementException("Tour does not exist " + tourId));
-    }
-
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler(NoSuchElementException.class)
-    public String return400(NoSuchElementException ex) {
-        return ex.getMessage();
-    }
-
     @GetMapping
     public List<RatingDto> getAllRatingsForTour(@PathVariable(value = "tourId") int tourId) {
         verifyTour(tourId);
@@ -62,5 +51,52 @@ public class TourRatingController {
         return Map.of("average", tourRatingRepository.findByPkTourId(tourId).stream()
                 .mapToInt(TourRating::getScore).average()
                 .orElseThrow(()-> new NoSuchElementException("Tour has no Ratings")));
+    }
+
+    @PutMapping
+    public RatingDto updateWithPut(@PathVariable(value = "tourId") int tourId,
+                                   @RequestBody @Validated RatingDto ratingDto) {
+        TourRating rating = verifyTourRating(tourId, ratingDto.getCustomerId());
+        rating.setScore(ratingDto.getScore());
+        rating.setComment(ratingDto.getComment());
+        return new RatingDto(tourRatingRepository.save(rating));
+    }
+
+    @PatchMapping
+    public RatingDto updateWithPatch(@PathVariable(value = "tourId") int tourId,
+                           @RequestBody @Validated RatingDto ratingDto) {
+        TourRating rating = verifyTourRating(tourId, ratingDto.getCustomerId());
+        if (ratingDto.getScore() != null) {
+            rating.setScore(ratingDto.getScore());
+        }
+        if (ratingDto.getComment() != null) {
+            rating.setComment(ratingDto.getComment());
+        }
+        return new RatingDto(tourRatingRepository.save(rating));
+    }
+
+    @DeleteMapping(path = "/{customerId}")
+    public void delete(@PathVariable(value = "tourId") int tourId,
+                       @PathVariable(value = "customerId") int customerId) {
+        TourRating rating = verifyTourRating(tourId, customerId);
+        tourRatingRepository.delete(rating);
+    }
+
+    private Tour verifyTour(int tourId) throws NoSuchElementException {
+        return tourRepository.findById(tourId).orElseThrow(() ->
+                new NoSuchElementException("Tour does not exist " + tourId));
+    }
+
+    private TourRating verifyTourRating(int tourId, int customerId) throws NoSuchElementException {
+        return tourRatingRepository.findByPkTourIdAndPkCustomerId(tourId, customerId)
+                .orElseThrow(()->new NoSuchElementException(
+                        "Tour-Rating pair for request(" + tourId + " for customer" + customerId)
+                );
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NoSuchElementException.class)
+    public String return400(NoSuchElementException ex) {
+        return ex.getMessage();
     }
 }
